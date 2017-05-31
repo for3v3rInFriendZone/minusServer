43f91af1988cefea7dffa6c0b1c1d2385d69c47d
DejanUrosevic <dejansuca@gmail.com>
2017-05-31 13:35:37 +0200

    dasda

--- src/main/java/com/minusServer/www/app/conf/AppConfig.java
+++ src/main/java/com/minusServer/www/app/conf/AppConfig.java
@@ -1,7 +1,12 @@
 package com.minusServer.www.app.conf;
 
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -11,4 +16,11 @@ public class AppConfig {
 	public BCryptPasswordEncoder passwordEncoder(){
 		return new BCryptPasswordEncoder();
 	}
+	
+	@Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
 }

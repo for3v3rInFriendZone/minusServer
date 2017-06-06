7c5ea0c0e0cf391bbb7daeccd4232f7752c86292
DejanUrosevic <dejansuca@gmail.com>
2017-06-06 14:24:17 +0200

    Ddas

--- src/main/java/com/minusServer/www/app/conf/AppConfig.java
+++ src/main/java/com/minusServer/www/app/conf/AppConfig.java
@@ -4,23 +4,17 @@ import javax.sql.DataSource;
 
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class AppConfig {
+public class AppConfig{ 
 
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder(){
 		return new BCryptPasswordEncoder();
 	}
-	
-	@Bean
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
-    }
 }
